61b13306031dcc7585966443cae455be6ae6e6ac
Anton Zhiliakov <gofranich@gmail.com>
2022-07-25 19:52:11 +0600

    Add change

---
+++ app.js
@@ -0,0 +1,18 @@
+const App = {
+    data(){
+        return{
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: ''
+        }
+    },
+    methods:{
+        inputChangeHandler(event){
+            //console.log('inputChangeHandler', event.target.value)
+            this.inputValue =  event.target.value
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
+
