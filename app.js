2e04d176a341be4f50fe9db1436b6a862f1f25c8
Kolamin <gofranich@gmail.com>
2022-07-26 17:45:19 +0600

    Add computed

--- app.js
+++ app.js
@@ -3,13 +3,36 @@ const App = {
         return{
             placeholderString: 'Введите название заметки',
             title: 'Список заметок',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
         }
     },
     methods:{
-        inputChangeHandler(event){
-            //console.log('inputChangeHandler', event.target.value)
-            this.inputValue =  event.target.value
+        addNewNote(){
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+
+        },
+        toUpperCase(item){
+           return item.toUpperCase()
+        },
+        removeNote(idx){
+           this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed(){
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
+        }
+    },
+    watch:{
+        inputValue(value){
+            if (value.length > 10){
+                this.inputValue = ''
+            }
         }
     }
 }
